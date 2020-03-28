3c09119454ec0b24c62216703a2bb411ec8ebeac
shiyutong123 <inexhausstible7@sina.com>
2020-03-28 19:46:42 +0800

    replace

--- https.js
+++ https.js
@@ -1,24 +1,34 @@
-
-const https = require('https');
+const https = require('https'); //导入模块
 const tls = require('tls');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; //主机名
+const port = 3000; //端口
 
-const options = {
-    key: fs.readFileSync('C:/Users/Administrator/ryans-key.pem'),
-    cert: fs.readFileSync('C:/Users/Administrator/ryans-cert.pem')
+const options1 = {
+    key: fs.readFileSync('C:/Users/Administrator/ryans1-key.pem'), //钥匙
+    cert: fs.readFileSync('C:/Users/Administrator/ryans1-cert.pem') //证书
+  };
+const options2 = {
+    key: fs.readFileSync('C:/Users/Administrator/ryans2-key.pem'), //钥匙
+    cert: fs.readFileSync('C:/Users/Administrator/ryans2-cert.pem') //证书
   };
 
-const server = https.createServer(options , (req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
+  //创建一个HTTP服务器
+const server = https.createServer(options1 , (req,res) => {
+    res.statusCode = 200;  //状态码
+    res.setHeader('Content-Type','text/plain');  //设置响应头
+    res.end('Hello World'); //快速结束没有任何数据的响应
 
 });
 
+
+setTimeout(function(){
+    server.setSecureContext(options2);
+},5000);
+
+//监听
 server.listen(port,hostname,()=>{
-    console.log(`Server running at https://${hostname}:${port}/`);
+    console.log(`Server running at https://${hostname}:${port}/`); //输出log
     
 });
